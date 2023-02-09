2df52b7689d90fb8728900f872ab22d41ea7c7eb
starlingvibes <starlingroot@protonmail.com>
2023-02-09 14:37:23 +0100

    creating a table of unique attributes

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- create table with unique attributes
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255)
+);
