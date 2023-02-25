113ff516aca59745b2cd627d2388cbe0e34657a4
starlingvibes <starlingroot@protonmail.com>
2023-02-25 15:14:22 +0100

    reading from redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -24,3 +24,32 @@ class Cache:
         self._redis.set(data_key, data)
         return data_key
 
+    def get(self, key: str, fn: Union[None, Callable] = None) -> Union[str, bytes, int, float]:
+        """Takes a string key argument and an optional callable argument named fn
+        """
+        if fn is None:
+            return self._redis.get(key)
+        return fn(self._redis.get(key))
+
+    def get_str(self, key: str) -> str:
+        """Takes a string key argument and returns a string
+        """
+        return self._redis.get(key).decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        """Takes a string key argument and returns an int
+        """
+        return int(self._redis.get(key))
+
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
