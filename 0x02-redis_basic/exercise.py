a500b095d7c39dde4191f012abef4f2c2d045ea3
starlingvibes <starlingroot@protonmail.com>
2023-02-25 15:33:07 +0100

    incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,6 +5,19 @@
 import redis
 import uuid
 from typing import Union, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """Counts how many times methods of the Cache class are called
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper
+        """
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -17,6 +30,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Takes a data argument and returns a string
         """
